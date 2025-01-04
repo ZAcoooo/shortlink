10c2dddd17aa05c6f64ac95ffdf3c2ea5b9cd3fb
Qichen Wang <souasgard@outlook.com>
2025-01-04 21:20:23 +0800

    refactor: improve user retrieval logic and enhance error handling

--- admin/src/main/java/io/github/zacoooo/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/io/github/zacoooo/shortlink/admin/controller/UserController.java
@@ -1,5 +1,6 @@
 package io.github.zacoooo.shortlink.admin.controller;
 
+import io.github.zacoooo.shortlink.admin.common.convention.result.Result;
 import io.github.zacoooo.shortlink.admin.dto.resp.UserRespDTO;
 import io.github.zacoooo.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +21,12 @@ public class UserController {
      * 根据用户名查询用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public UserRespDTO getUserByUsername(@PathVariable("username") String username) {
-        return userService.getUserByUsername(username);
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
+        UserRespDTO result = userService.getUserByUsername(username);
+        if (result == null) {
+            return new Result<UserRespDTO>().setCode("-1").setMessage("用户查询为空");
+        } else {
+            return new Result<UserRespDTO>().setCode("0").setData(result);
+        }
     }
 }
