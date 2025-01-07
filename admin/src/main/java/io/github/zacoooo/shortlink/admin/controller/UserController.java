a90b3f44a10a949230864029f2d1575aa04ba324
Qichen Wang <souasgard@outlook.com>
2025-01-07 18:17:38 +0800

    feat: add hasUsername endpoint

--- admin/src/main/java/io/github/zacoooo/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/io/github/zacoooo/shortlink/admin/controller/UserController.java
@@ -9,6 +9,7 @@ import io.github.zacoooo.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -34,5 +35,13 @@ public class UserController {
     @GetMapping("/api/shortlink/v1/actual/user/{username}")
     public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username) {
         return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
+    }
+
+    /**
+     * 查询用户名是否存在
+     */
+    @GetMapping("/api/short-link/admin/v1/user/has-username")
+    public Result<Boolean> hasUsername(@RequestParam("username") String username) {
+        return Results.success(userService.hasUsername(username));
     }
 }
