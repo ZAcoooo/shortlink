c06b75d923a666fa7cf0f55d786b3cb71597b48b
Qichen Wang <souasgard@outlook.com>
2025-01-10 22:32:36 +0800

    feat: add user register lock

---
+++ admin/src/main/java/io/github/zacoooo/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,8 @@
+package io.github.zacoooo.shortlink.admin.common.constant;
+
+public class RedisCacheConstant {
+    /**
+     * 用户注册分布式锁
+     */
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
