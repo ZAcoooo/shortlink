87586bc6a1fe04ce1b0a35277cf2db693ccbf923
Qichen Wang <souasgard@outlook.com>
2025-01-15 21:39:19 +0800

    feat: group service methods

--- admin/src/main/java/io/github/zacoooo/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/io/github/zacoooo/shortlink/admin/service/GroupService.java
@@ -12,20 +12,13 @@ import java.util.List;
  * 短链接分组接口层
  */
 public interface GroupService extends IService<GroupDO> {
+
     /**
      * 新增短链接分组
      *
      * @param groupName 短链接分组名
      */
     void saveGroup(String groupName);
-
-    /**
-     * 新增短链接分组
-     *
-     * @param username  用户名
-     * @param groupName 短链接分组名
-     */
-    void saveGroup(String username, String groupName);
 
     /**
      * 查询用户短链接分组集合
