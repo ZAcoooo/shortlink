4b186ad375b91fc2915237215f8180b3df666c90
Qichen Wang <souasgard@outlook.com>
2025-01-18 03:47:55 +0800

    feat: initialize ShortLink

---
+++ project/src/main/java/io/github/zacoooo/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -0,0 +1,10 @@
+package io.github.zacoooo.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.github.zacoooo.shortlink.project.dao.entity.ShortLinkDO;
+
+/**
+ * 短链接持久层
+ */
+public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+}
