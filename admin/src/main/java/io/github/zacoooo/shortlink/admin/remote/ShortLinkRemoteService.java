c2a97ba23ac903157f9c9a1c3b624b41fcfe597e
ZAcoooo <souasgard@Outlook.com>
2025-01-21 04:17:28 +0800

    perf: optimize ShortLinkRemoteService package path

--- admin/src/main/java/io/github/zacoooo/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/io/github/zacoooo/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -1,4 +1,4 @@
-package io.github.zacoooo.shortlink.admin.remote.dto;
+package io.github.zacoooo.shortlink.admin.remote;
 
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson2.JSON;
@@ -8,9 +8,11 @@ import io.github.zacoooo.shortlink.admin.common.convention.result.Result;
 import io.github.zacoooo.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import io.github.zacoooo.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
 import io.github.zacoooo.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import io.github.zacoooo.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import io.github.zacoooo.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -42,6 +44,20 @@ public interface ShortLinkRemoteService {
         requestMap.put("current", requestParam.getCurrent());
         requestMap.put("size", requestParam.getSize());
         String resultPageStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/page", requestMap);
+        return JSON.parseObject(resultPageStr, new TypeReference<>() {
+        });
+    }
+
+    /**
+     * 查询分组短链接总量
+     *
+     * @param requestParam 分组短链接总量请求参数
+     * @return 查询分组短链接总量响应
+     */
+    default Result<List<ShortLinkGroupCountQueryRespDTO>> listGroupShortLinkCount(List<String> requestParam) {
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("requestParam", requestParam);
+        String resultPageStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/count", requestMap);
         return JSON.parseObject(resultPageStr, new TypeReference<>() {
         });
     }
