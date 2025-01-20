565c6a40bc5e5790917e0f8a6a56f4cea74e38c0
ZAcoooo <souasgard@Outlook.com>
2025-01-21 04:26:40 +0800

    feature: add default group after user register

--- admin/src/main/java/io/github/zacoooo/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/io/github/zacoooo/shortlink/admin/service/impl/UserServiceImpl.java
@@ -16,6 +16,7 @@ import io.github.zacoooo.shortlink.admin.dto.req.UserRegisterReqDTO;
 import io.github.zacoooo.shortlink.admin.dto.req.UserUpdateReqDTO;
 import io.github.zacoooo.shortlink.admin.dto.resp.UserLoginRespDTO;
 import io.github.zacoooo.shortlink.admin.dto.resp.UserRespDTO;
+import io.github.zacoooo.shortlink.admin.service.GroupService;
 import io.github.zacoooo.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
@@ -42,6 +43,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
     private final RedissonClient redissonClient;
     private final StringRedisTemplate stringRedisTemplate;
+    private final GroupService groupService;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -78,6 +80,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
                     throw new ClientException(USER_EXIST);
                 }
                 userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                groupService.saveGroup("默认分组");
                 return;
             }
             throw new ClientException(USER_NAME_EXIST);
