7784fbbab995d2254e0e15b7a1a0ab465ce692c5
ZAcoooo <souasgard@Outlook.com>
2025-02-05 20:56:07 +0800

    feature: 开发短链接跳转功能

---
+++ project/src/main/java/io/github/zacoooo/shortlink/project/dao/mapper/ShortLinkGotoMapper.java
@@ -0,0 +1,10 @@
+package io.github.zacoooo.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.github.zacoooo.shortlink.project.dao.entity.ShortLinkGotoDO;
+
+/**
+ * 短链接跳转持久层
+ */
+public interface ShortLinkGotoMapper extends BaseMapper<ShortLinkGotoDO> {
+}
