f5362d9bcd8f31f547eebaf6b34f40d11a42f3f2
ZAcoooo <souasgard@Outlook.com>
2025-02-10 01:50:05 +0800

    feature: 开发短链接分页监控相关功能

---
+++ project/src/main/java/io/github/zacoooo/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -0,0 +1,10 @@
+package io.github.zacoooo.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.github.zacoooo.shortlink.project.dao.entity.LinkStatsTodayDO;
+
+/**
+ * 短链接今日统计持久层
+ */
+public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+}
